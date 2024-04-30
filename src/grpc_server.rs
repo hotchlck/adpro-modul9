0378611d0eddae91fa6fe221c1d47b6299743a10
Salma Kurnia Dewi <salma.kurnia@ui.ac.id>
2024-04-30 10:55:49 +0700

    Chat Service Implementation

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -14,12 +14,6 @@ pub mod services{
 #[derive(Default)]
 pub struct MyPaymentService{}
 
-#[derive(Default)]
-pub struct MyTransactionService{}
-
-#[derive(Default)]
-pub struct MyChatService{}
-
 #[tonic::async_trait]
 impl PaymentService for MyPaymentService{
     async fn process_payment(
@@ -32,21 +26,8 @@ impl PaymentService for MyPaymentService{
     }
 }
 
-#[tokio::main]
-async fn main()->Result<(), Box<dyn std::error::Error>>{
-    let addr = "[::1]:50051".parse()?;
-    let payment_service = MyPaymentService::default();
-    let transaction_service = MyTransactionService::default();
-    let chat_service = MyChatService::default();
-
-    Server::builder()
-        .add_service(PaymentServiceServer::new(payment_service))
-        .add_service(TransactionServiceServer::new(transaction_service))
-        .add_service(ChatServiceServer::new(chat_service))
-        .serve(addr)
-        .await?;
-    Ok(())
-}
+#[derive(Default)]
+pub struct MyTransactionService{}
 
 #[tonic::async_trait]
 impl TransactionService for MyTransactionService{
@@ -75,6 +56,9 @@ impl TransactionService for MyTransactionService{
     }
 }
 
+#[derive(Default)]
+pub struct MyChatService{}
+
 #[tonic::async_trait]
 impl ChatService for MyChatService{
     type ChatStream = ReceiverStream<Result<ChatMessage, Status>>;
@@ -100,4 +84,20 @@ impl ChatService for MyChatService{
         });
         Ok(Response::new(ReceiverStream::new(rx)))
     }
+}
+
+#[tokio::main]
+async fn main()->Result<(), Box<dyn std::error::Error>>{
+    let addr = "[::1]:50051".parse()?;
+    let payment_service = MyPaymentService::default();
+    let transaction_service = MyTransactionService::default();
+    let chat_service = MyChatService::default();
+
+    Server::builder()
+        .add_service(PaymentServiceServer::new(payment_service))
+        .add_service(TransactionServiceServer::new(transaction_service))
+        .add_service(ChatServiceServer::new(chat_service))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
